151918ad4a44a4d15982efcf6c921d95caaeeff8
Michael Walker <mike@barrucadu.co.uk>
2014-05-06 02:12:26 +0100

    Add skeelton fractal renderer

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <curses.h>
+#include <complex.h>
 #include <stdbool.h>
+
+#include "mandlebrot.h"
+
+/**
+ * Render a fractal to the screen, colouring by how many points per
+ * pixel are in the farctal.
+ */
+static void render_fractal(bool (*in_fractal) (complex double),
+                           double complex topleft,
+                           double complex bottomright) {
+  (void) in_fractal;
+  (void) topleft;
+  (void) bottomright;
+}
 
 int main() {
   WINDOW * mainwin = initscr();
@@ -10,6 +25,11 @@ int main() {
   intrflush(stdscr, false);
   keypad(stdscr, true);
   curs_set(0);
+
+  render_fractal(&in_mandlebrot,
+                 -5.0 + 5.0 * I,
+                 5.0 - 5.0 * I);
+  getch();
 
   curs_set(1);
   nl();
