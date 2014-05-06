99cfbeb2c60783fe3c0d8602471ca81f3de4a0e2
Michael Walker <mike@barrucadu.co.uk>
2014-05-07 00:59:27 +0100

    Rename variables in mandlebrot code to more closely reflect convention

--- mandlebrot.c
+++ mandlebrot.c
@@ -20,7 +20,7 @@ bool in_mandlebrot(double complex z, const char* argv[], int argn) {
  * Multibrot is like Mandlebrot, but rather than z^2 we sue z^d, where
  * d is a parameter.
  */
-bool in_multibrot(double complex z, const char *argv[], int argn) {
+bool in_multibrot(double complex c, const char *argv[], int argn) {
   // Get the parameter, falling back to d=2 if it's not given
   double d;
   if(argn == 0) {
@@ -29,14 +29,14 @@ bool in_multibrot(double complex z, const char *argv[], int argn) {
     sscanf(argv[0], "%lf", &d);
   }
 
-  double complex zi = z;
+  double complex z = c;
   double re;
   double im;
 
   for(unsigned int i = 0; i < MAX_ITERATIONS; i++) {
-    zi = cpow(zi, d) + z;
-    re = creal(zi);
-    im = cimag(zi);
+    z = cpow(z, d) + c;
+    re = creal(z);
+    im = cimag(z);
 
     if(re * re + im * im > 4) {
       return false;
