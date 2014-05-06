b0f70bc4754bea6d6e6938ec5c7d1a2a79aa27b6
Michael Walker <mike@barrucadu.co.uk>
2014-05-06 02:16:10 +0100

    Get dimensions of window

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 #include "mandlebrot.h"
 
 /**
+ * Size of square area to render in.
+ */
+int screen_size;
+
+/**
  * Render a fractal to the screen, colouring by how many points per
  * pixel are in the farctal.
  */
@@ -26,6 +31,14 @@ int main() {
   keypad(stdscr, true);
   curs_set(0);
 
+  // Get terminal size
+  int max_y, max_x;
+  getmaxyx(mainwin, max_y, max_x);
+
+  // Set size of rendering area
+  screen_size = (max_y < max_x) ? max_y : max_x;
+
+  // Render a fractal
   render_fractal(&in_mandlebrot,
                  -5.0 + 5.0 * I,
                  5.0 - 5.0 * I);
